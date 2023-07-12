ccc282b136d09f9e3c88a86f79bd1dfdc8edf215
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-07-12 17:49:47 +0530

    update nav bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,44 +4,47 @@ import Image from "next/image";
 import { AuthContext } from "@/services/auth.context";
 
 export default function Navbar() {
-  const { user, login, logout } = useContext(AuthContext);
+  const { user, login, logout, authReady } = useContext(AuthContext);
   console.log({ user });
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
-        <ul>
-          <li>
-            <Link href="/" legacyBehavior>
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/guides" legacyBehavior>
-              <a>Guides</a>
-            </Link>
-          </li>
-          {user && (
+
+        {authReady && (
+          <ul>
             <li>
-              <Link href="/#" legacyBehavior>
-                <a>
-                  {user.user_metadata.full_name} <small>({user.email})</small>
-                </a>
+              <Link href="/" legacyBehavior>
+                <a>Home</a>
               </Link>
             </li>
-          )}
-          {!user && (
-            <li onClick={login} className="btn">
-              Login/ Signup
+            <li>
+              <Link href="/guides" legacyBehavior>
+                <a>Guides</a>
+              </Link>
             </li>
-          )}
-          {user && (
-            <li onClick={logout} className="btn">
-              Logout
-            </li>
-          )}
-        </ul>
+            {user && (
+              <li>
+                <Link href="/#" legacyBehavior>
+                  <a>
+                    {user.user_metadata.full_name} <small>({user.email})</small>
+                  </a>
+                </Link>
+              </li>
+            )}
+            {!user && (
+              <li onClick={login} className="btn">
+                Login/ Signup
+              </li>
+            )}
+            {user && (
+              <li onClick={logout} className="btn">
+                Logout
+              </li>
+            )}
+          </ul>
+        )}
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
--- services/auth.context.js
+++ services/auth.context.js
@@ -9,6 +9,7 @@ export const AuthContext = createContext({
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [authReady, setAuthReady] = useState(false);
 
   useEffect(() => {
     netlifyIdentity.on("login", (user) => {
@@ -22,9 +23,15 @@ export const AuthContextProvider = ({ children }) => {
       console.log("logout event");
     });
 
+    netlifyIdentity.on("init", (user) => {
+      setUser(user);
+      setAuthReady(true);
+      console.log("init event");
+    });
+
     //init netlify identity connection
     netlifyIdentity.init();
-    
+
     return () => {
       netlifyIdentity.off("login");
       netlifyIdentity.off("logout");
@@ -38,7 +45,7 @@ export const AuthContextProvider = ({ children }) => {
   const logout = () => {
     netlifyIdentity.logout();
   };
-  const context = { user, login, logout };
+  const context = { user, login, logout, authReady };
   return (
     <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
   );
