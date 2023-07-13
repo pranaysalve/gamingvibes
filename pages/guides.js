1e00f6e67fb0b3565e1d674f490fb2eb86118fc9
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-07-13 14:47:49 +0530

    update authentication with context

--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,61 @@
-import styles from '../styles/Guides.module.css'
+import { AuthContext } from "@/services/auth.context";
+import { useEffect, useContext, useState } from "react";
+import styles from "../styles/Guides.module.css";
 
 export default function Guides() {
+  const { user, authReady } = useContext(AuthContext);
+
+  const [guides, setGuide] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (authReady) {
+      fetch(
+        "/.netlify/functions/guides",
+        user && {
+          headers: { Authorization: "Bearer " + user.token.access_token },
+        }
+      )
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("You must login to view this page");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setGuide(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setGuide(null);
+        });
+    }
+  }, [user, authReady]);
+
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
-    </div> 
-  )
+      {!authReady && <div>Loading...</div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{error}</p>
+        </div>
+      )}
+
+      {guides &&
+        guides.map((guide) => (
+          <div key={guide.title} className={styles.card}>
+            <h3>{guide.title}</h3>
+            <h4>written by {guide.author}</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. At
+              corrupti iste ab magnam dignissimos id maxime rerum quae minima.
+              Delectus maxime culpa est consequatur veritatis, perspiciatis cum
+              corrupti possimus quis?
+            </p>
+          </div>
+        ))}
+    </div>
+  );
 }
